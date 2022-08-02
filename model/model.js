e482ccc604d68b3f41aad56d60e19736bf4066a6
pippaamy <pippa_austin@live.com>
2022-08-02 10:32:45 +0100

    ticket 4

--- model/model.js
+++ model/model.js
@@ -1,10 +1,19 @@
+const { rawListeners } = require("../db/connection");
 const db = require("../db/connection");
 
 
 exports.selectTopics = ()=> {
     return db.query("SELECT * FROM topics").then(({rows}) => {
-        //console.log(rows)
         return rows;
+    })
+}
+
+exports.selectArticleById =(id)=>{
+    return db.query("SELECT * FROM articles WHERE article_id = $1; ", [id]).then((res)=>{
+      if (res.rows.length === 0) {  
+                 return Promise.reject({ status: 404, msg: 'path not found' });
+          }
+        return res.rows[0];
     })
 }
 
